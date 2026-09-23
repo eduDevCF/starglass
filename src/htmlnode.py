9215f1fb2924c49e14914761620894778ca43229
edudevcf <cforan99@gmail.com>
2026-09-22 23:03:03 -0700

    updated typing

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,21 +1,27 @@
 
 
 class HTMLNode():
-    def __init__(self, tag: str = None, value: str = None, children = None, props = None):
+    def __init__(
+        self, 
+        tag: str | None = None, 
+        value: str | None = None, 
+        children: list["HTMLNode"] | None = None, 
+        props: dict[str, str] | None = None
+    ) -> None:
         self.tag = tag
         self.value = value
         self.children = children
         self.props = props
     
-    def to_html(self):
-        raise NotImplementedError()
+    def to_html(self) -> str:
+        raise NotImplementedError("to_html method not implemented")
     
-    def props_to_html(self):
+    def props_to_html(self) -> str:
         formatted = ""
         if self.props:
             for key, value in self.props:
                 formatted += f' {key}="{value}"'
         return formatted
 
-    def __repr__(self):
-        return f"{self.tag}  PROPS: {self.props}  VALUE: {self.value}  CHILDREN: {self.children}"
+    def __repr__(self) -> str:
+        return f"HTMLNode({self.tag}, {self.value}  CHILDREN: {self.children}  PROPS: {self.props})"
--- src/textnode.py
+++ src/textnode.py
@@ -10,12 +10,12 @@ class TextType(Enum):
 
 
 class TextNode:
-    def __init__(self, text, text_type, url=None):
+    def __init__(self, text: str, text_type: str, url: str | None = None):
         self.text = text
         self.text_type = text_type
         self.url = url
 
-    def __eq__(self, other) -> bool:
+    def __eq__(self, other: object) -> bool:
         if (self.text == other.text 
             and self.text_type == other.text_type 
             and self.url == other.url):
