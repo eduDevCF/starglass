dd4f75df2d6b6969e96aacaf22cd6eafcb864eeb
edudevcf <cforan99@gmail.com>
2026-09-25 01:23:03 -0700

    added a BlockType Enum and updated parse_markdown functions to filter out code blocks first, updated tests, wrote blocks_to_block_type function and tests

--- src/parse_markdown.py
+++ src/parse_markdown.py
@@ -1,20 +1,88 @@
 import re
+from enum import Enum
+
+class BlockType(Enum):
+    P = "paragraph"
+    H1 = "heading1"
+    H2 = "heading2"
+    H3 = "heading3"
+    H4 = "heading4"
+    H5 = "heading5"
+    H6 = "heading6"
+    CODE = "code"
+    QUOTE = "quote"
+    UL = "unordered list"
+    OL = "ordered list"
+
+
+def extract_code_blocks(text: str) -> list[str]:
+    #Todo: Write tests
+    blocks = []
+    code_blocks = text.split("```")
+
+    for i in range(len(code_blocks)):
+        if code_blocks[i] == "":
+            continue
+        if i % 2 == 1:
+            code_block = "```" + code_blocks[i]
+            blocks.append(code_block)
+        if i % 2 == 0:
+            blocks.append(code_blocks[i])
+    
+    return blocks
+
+
+def markdown_to_blocks(text: str) -> list[str]:
+    #Todo: UPDATE TESTS to include code blocks
+    code_blocks: list[str] = extract_code_blocks(text)
+
+    blocks = []
+    for item in code_blocks:
+        if item[0:4] == "```\n":
+            blocks.append(item)
+        else:
+            text_blocks = item.split("\n\n")
+            for block in text_blocks:
+                polished_block = block.strip()
+                if polished_block == "":
+                    continue
+                blocks.append(polished_block)
+
+    return blocks
+
+
+def block_to_block_type(text: str) -> BlockType:
+    # Todo: write tests
+    if text.startswith("###### "):
+        return BlockType.H6
+    if text.startswith("##### "):
+        return BlockType.H5
+    if text.startswith("#### "):
+        return BlockType.H4
+    if text.startswith("### "):
+        return BlockType.H3
+    if text.startswith("## "):
+        return BlockType.H2
+    if text.startswith("# "):
+        return BlockType.H1
+    if text.startswith("```\n"):
+        return BlockType.CODE
+    if text.startswith(">"):
+        return BlockType.QUOTE
+    if text.startswith("- "):
+        return BlockType.UL
+    if re.match(r"(\d. )", text):
+        return BlockType.OL
+    return BlockType.P
+
 
 def extract_markdown_images(text: str) -> list[tuple]:
     matches = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
     return matches
+    
 
 def extract_markdown_links(text: str) -> list[tuple]:
     matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
     return matches
 
-def markdown_to_blocks(text: str) -> list[str]:
-    raw_blocks = text.split("\n\n")
-    blocks = []
-    for block in raw_blocks:
-        polished_block = block.strip()
-        if polished_block == "":
-            continue
-        blocks.append(block)
-    return blocks
 
--- src/test_parse_markdown.py
+++ src/test_parse_markdown.py
@@ -1,5 +1,8 @@
 import unittest
-from parse_markdown import extract_markdown_images, extract_markdown_links, markdown_to_blocks
+from parse_markdown import (
+    extract_markdown_images, extract_markdown_links, BlockType,
+    markdown_to_blocks, extract_code_blocks, block_to_block_type
+)
 
 class TestParseMarkdown(unittest.TestCase):
     def test_extract_markdown_images(self):
@@ -14,6 +17,41 @@ class TestParseMarkdown(unittest.TestCase):
         )
         self.assertListEqual([("link text", "https://imgur.com")], matches)
     
+    def test_extract_code_blocks(self):
+        md = """## Heading2 about `if... else` blocks
+
+This is how to use conditional logic in Python.
+
+```
+
+username = "Boots"
+
+if len(username) > 0:
+  print(f"Welcome, {username}!")
+else:
+  print("Error: Username cannot be empty")
+
+```
+
+    You can also add an `elif` statement in there."""
+
+        md_code = """```
+
+username = "Boots"
+
+if len(username) > 0:
+  print(f"Welcome, {username}!")
+else:
+  print("Error: Username cannot be empty")
+
+"""
+        blocks = extract_code_blocks(md)
+
+        self.assertEqual(len(blocks), 3)
+        self.assertEqual(blocks[0], "## Heading2 about `if... else` blocks\n\nThis is how to use conditional logic in Python.\n\n")
+        self.assertEqual(blocks[1], md_code)
+        self.assertEqual(blocks[2], "\n\n    You can also add an `elif` statement in there.")
+
     def test_markdown_to_blocks(self):
         md = """This is **bolded** paragraph
 
@@ -31,6 +69,68 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+
+    def test_markdown_to_blocks_with_code(self):
+        md = """## Heading2 about `if... else` blocks
+
+This is how to use conditional logic in Python.
+
+```
+
+username = "Boots"
+
+if len(username) > 0:
+  print(f"Welcome, {username}!")
+else:
+  print("Error: Username cannot be empty")
+
+```
+
+    You can also add an `elif` statement in there."""
+
+        md_code = """```
+
+username = "Boots"
+
+if len(username) > 0:
+  print(f"Welcome, {username}!")
+else:
+  print("Error: Username cannot be empty")
+
+"""
+
+        blocks = markdown_to_blocks(md)
+
+        self.assertEqual(len(blocks), 4)
+        self.assertEqual(blocks[0], "## Heading2 about `if... else` blocks")
+        self.assertEqual(blocks[1], "This is how to use conditional logic in Python.")
+        self.assertEqual(blocks[2], md_code)
+        self.assertEqual(blocks[3], "You can also add an `elif` statement in there.")
+
+    def test_block_to_block_type(self):
+        h1 = "# Heading 1"
+        h2 = "## Heading 2"
+        h3 = "### Heading 3"
+        h4 = "#### Heading 4"
+        h5 = "##### Heading 5"
+        h6 = "###### Heading 6"
+        code = "```\nprint('Hello World')\n"
+        quote = ">A wise woman once said..."
+        ul = "- Item 1\n- Item 2\n- Item 3"
+        ol = "1. First\n2. Second\n3. Third"
+        p = "This is a vanilla paragraph."
+        self.assertEqual(block_to_block_type(h1), BlockType.H1)
+        self.assertEqual(block_to_block_type(h2), BlockType.H2)
+        self.assertEqual(block_to_block_type(h3), BlockType.H3)
+        self.assertEqual(block_to_block_type(h4), BlockType.H4)
+        self.assertEqual(block_to_block_type(h5), BlockType.H5)
+        self.assertEqual(block_to_block_type(h6), BlockType.H6)
+        self.assertEqual(block_to_block_type(code), BlockType.CODE)
+        self.assertEqual(block_to_block_type(quote), BlockType.QUOTE)
+        self.assertEqual(block_to_block_type(ul), BlockType.UL)
+        self.assertEqual(block_to_block_type(ol), BlockType.OL)
+        self.assertEqual(block_to_block_type(p), BlockType.P)
 
 
 if __name__ == "__main__":
